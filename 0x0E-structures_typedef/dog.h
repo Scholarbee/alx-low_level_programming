39bd830a39165313ee23022948344c31e73d99e2
Scholar <scholarbee6@gmail.com>
2023-07-17 10:25:23 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - My name is dog and i am defining myself
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Return: a call to the member in the struct
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+/* additiona; functions */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
